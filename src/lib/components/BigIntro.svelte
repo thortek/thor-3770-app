53e2866001a0fb8465bc2668d383d6fbbbfd3774
Thor Anderson <ta.anderson@gmail.com>
2025-08-25 15:41:59 -0600

    Added the tutorial route and some introduction code samples

---
+++ src/lib/components/BigIntro.svelte
@@ -0,0 +1,3 @@
+<div class="h-96 flex flex-col justify-center items-center text-center px-4 bg-amber-100">
+    <h1 class="text-4xl md:text-5xl font-bold mb-4">Welcome to Our Application!</h1>
+</div>
---
+++ src/routes/tutorial/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  // You can add any necessary script logic here
+  import BigIntro from "$lib/components/BigIntro.svelte";
+  let src = "PikaThorAnime.png";
+  let contentMessage = `This is the tutorial <strong>page content.</strong> It can include HTML tags.`;
+</script>
+
+<h1 class="text-blue-600 font-bold text-5xl text-center">Welcome to the tutorial page</h1>
+<BigIntro />
+<p class="text-center">{@html contentMessage}</p>
+<img {src} alt="Pikachu with Thor's Hammer" class="mx-auto mt-4" />
