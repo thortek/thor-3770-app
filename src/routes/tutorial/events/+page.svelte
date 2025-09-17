2c788c194c7512782fc780fcfb433c5cfeedc367
Thor Anderson <ta.anderson@gmail.com>
2025-09-17 15:42:37 -0600

    Added events route to tutorial

---
+++ src/routes/tutorial/events/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+let m = $state({ x: 0, y: 0})
+
+function handleMouseMove(event: MouseEvent) {
+    m.x = event.clientX
+    m.y = event.clientY
+}
+
+
+</script>
+
+<div class="w-96 h-96 border" onpointermove={handleMouseMove}>
+    The pointer is at {Math.round(m.x)} x {Math.round(m.y)}
+</div>
