199ccddc1bcc05a2c9c8682fd83ac8d00cc5977a
Thor Anderson <ta.anderson@gmail.com>
2025-10-15 15:32:30 -0600

    Added a Progress with Tween and an SVG Circle with Spring animation

--- src/routes/tutorial/motion/+page.svelte
+++ src/routes/tutorial/motion/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { fade, fly, slide } from 'svelte/transition'
+	import { Progress } from '@skeletonlabs/skeleton-svelte'
+	import { Tween, Spring } from 'svelte/motion'
+	import { cubicOut } from 'svelte/easing'
 
 	let checkboxVisible = $state(true)
 	let flyVisible = $state(true)
@@ -9,6 +12,23 @@
 	let items = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
 	let showItems = $state(true)
 	let i = $state(5)
+
+	let progress = $state(
+		new Tween(0, {
+			duration: 400,
+			easing: cubicOut
+		})
+	)
+
+	let coords = new Spring(
+		{ x: 50, y: 50 },
+		{
+			stiffness: 0.1,
+			damping: 0.25
+		}
+	)
+
+	let size = new Spring(10)
 
 	function typewriter(node: HTMLElement, { speed = 1 }) {
 		const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE
@@ -62,48 +82,131 @@
 			{/if}
 		</div>
 	</div>
-	<label>
-		<input type="checkbox" bind:checked={typewriterVisible} />
-		Typewriter effect visible
-	</label>
+	<div class="flex w-full gap-4">
+		<div class="w-1/2 space-y-4 p-4">
+			<label>
+				<input type="checkbox" bind:checked={typewriterVisible} />
+				Typewriter effect visible
+			</label>
 
-	{#if typewriterVisible}
-		<p class="rounded-xl border p-1 shadow-lg" transition:typewriter={{ speed: 2 }}>
-			The quick brown fox jumps over the lazy dog
-		</p>
-	{/if}
-	<hr class="w-1/2" />
-	<label>
-		<input type="checkbox" bind:checked={statusVisible} />
-		Status message state: {status}
-	</label>
+			{#if typewriterVisible}
+				<p class="rounded-xl border p-1 shadow-lg" transition:typewriter={{ speed: 2 }}>
+					The quick brown fox jumps over the lazy dog
+				</p>
+			{/if}
+			<hr />
+			<label>
+				<input type="checkbox" bind:checked={statusVisible} />
+				Status message state: {status}
+			</label>
 
-	{#if statusVisible}
-		<p
-			transition:fly={{ x: 400, duration: 1000 }}
-			onintrostart={() => (status = 'intro started')}
-			onoutrostart={() => (status = 'outro started')}
-			onintroend={() => (status = 'intro ended')}
-			onoutroend={() => (status = 'outro ended')}
-		>
-			Flies in and out
-		</p>
-	{/if}
-	<hr class="w-1/2" />
-	<label>
-		<input type="checkbox" bind:checked={showItems} />
-		show list
-	</label>
+			{#if statusVisible}
+				<p
+					transition:fly={{ x: 400, duration: 1000 }}
+					onintrostart={() => (status = 'intro started')}
+					onoutrostart={() => (status = 'outro started')}
+					onintroend={() => (status = 'intro ended')}
+					onoutroend={() => (status = 'outro ended')}
+				>
+					Flies in and out
+				</p>
+			{/if}
+			<hr />
+			<label>
+				<input type="checkbox" bind:checked={showItems} />
+				show list
+			</label>
 
-	<label>
-		<input type="range" bind:value={i} max="10" />
-	</label>
+			<label>
+				<input type="range" bind:value={i} max="10" />
+			</label>
 
-	{#if showItems}
-		{#each items.slice(0, i) as item}
-			<div class="w-1/2 border-t border-gray-200 p-0.5 text-center" transition:slide|global>
-				{item}
+			{#if showItems}
+				{#each items.slice(0, i) as item}
+					<div class="w-1/2 border-t border-gray-200 p-0.5 text-center" transition:slide|global>
+						{item}
+					</div>
+				{/each}
+			{/if}
+		</div>
+		<div class="w-1/2 space-y-4 p-4">
+			<Progress value={progress.current} />
+			<button
+				class="btn preset-filled-primary-950-50"
+				type="button"
+				onclick={() => progress.set(0)}
+			>
+				0%
+			</button>
+			<button
+				class="btn preset-filled-primary-950-50"
+				type="button"
+				onclick={() => progress.set(25)}
+				>25%
+			</button>
+			<button
+				class="btn preset-filled-primary-950-50"
+				type="button"
+				onclick={() => progress.set(50)}
+			>
+				50%
+			</button>
+			<button
+				class="btn preset-filled-primary-950-50"
+				type="button"
+				onclick={() => progress.set(75)}
+			>
+				75%
+			</button>
+			<button
+				class="btn preset-filled-primary-950-50"
+				type="button"
+				onclick={() => progress.set(100)}
+			>
+				100%
+			</button>
+
+			<div class="border">
+				<svg
+					class="top-0 left-0 h-full w-full"
+					onmousemove={(e) => {
+						const  {left, top} = e.currentTarget.getBoundingClientRect()
+						coords.target = { x: e.clientX - left, y: e.clientY - top }
+					}}
+					onmousedown={() => (size.target = 30)}
+					onmouseup={() => (size.target = 10)}
+					role="presentation"
+				>
+					<circle cx={coords.current.x} cy={coords.current.y} r={size.current} />
+				</svg>
+
+				
 			</div>
-		{/each}
-	{/if}
+			<div class="w-48 select-none">
+					<label>
+						<h3>stiffness ({coords.stiffness})</h3>
+						<input
+							class="input"
+							bind:value={coords.stiffness}
+							type="range"
+							min="0.01"
+							max="1"
+							step="0.01"
+						/>
+					</label>
+
+					<label>
+						<h3>damping ({coords.damping})</h3>
+						<input
+							class="input"
+							bind:value={coords.damping}
+							type="range"
+							min="0.01"
+							max="1"
+							step="0.01"
+						/>
+					</label>
+				</div>
+		</div>
+	</div>
 </div>
